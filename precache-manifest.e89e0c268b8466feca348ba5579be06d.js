927fbd12a0a178ae32dd45e5beb899e7e10c8587
Jamilur Rahman <abir2jamilur30@gmail.com>
2020-05-02 17:51:52 +0600

    Updates

---
+++ precache-manifest.e89e0c268b8466feca348ba5579be06d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda332a268938e99e59b54539f81b738",
+    "url": "/jr-fancy-ui/index.html"
+  },
+  {
+    "revision": "b41b7bd2c11100691264",
+    "url": "/jr-fancy-ui/static/css/main.14d6717c.chunk.css"
+  },
+  {
+    "revision": "fa38a347ff1970e56c6c",
+    "url": "/jr-fancy-ui/static/js/2.ca7ae83f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jr-fancy-ui/static/js/2.ca7ae83f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b41b7bd2c11100691264",
+    "url": "/jr-fancy-ui/static/js/main.0c650a3c.chunk.js"
+  },
+  {
+    "revision": "483a8fb637511c3d332d",
+    "url": "/jr-fancy-ui/static/js/runtime-main.1badc297.js"
+  },
+  {
+    "revision": "f0c37f18ddfe5eaacf1878cbe60f6362",
+    "url": "/jr-fancy-ui/static/media/1.f0c37f18.png"
+  },
+  {
+    "revision": "55fbfc9d1cf1f7e615cfee41deb49983",
+    "url": "/jr-fancy-ui/static/media/2.55fbfc9d.png"
+  }
+]);
